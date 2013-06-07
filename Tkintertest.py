a218fad3fb245c150d015e4a8f7de9d60fe16dee
Steve Skripnik <skripnik@gmail.com>
2013-06-07 12:36:13 -0400

    Add Tkinter test

---
+++ Tkintertest.py
@@ -0,0 +1,20 @@
+from Tkinter import Tk, Frame, BOTH
+
+class Example(Frame):
+    def __init__(self, parent):
+        Frame.__init__(self, parent, background = "white")
+        self.parent = parent
+        self.initUI()
+
+    def iniUI(self):
+        self.parent.title("Simple")
+        self.pack(fill=BOTH,expand=1)
+
+def main():
+    root = Tk()
+    root.geometry("250x150+300+300")
+    app = Example(root)
+    root.mainloop()
+
+if __name__ == '__main__':
+    main()
